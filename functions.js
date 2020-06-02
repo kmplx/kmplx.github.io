c07293b729cd1a4898cd19fdadd41dd1bdfb3ef9
kmplx <66211300+kmplx@users.noreply.github.com>
2020-06-02 08:51:09 -0400

    Initial Commit

--- functions.js
+++ functions.js
@@ -18,12 +18,30 @@ $(document).ready(function(){
         $("html,body").animate({
             scrollTop: $("html, body").offset().top
         }, 1500);
-    })
+    });
+
+    $("#btnPlayer").click(function(){
+        
+            $(".player").toggleClass('showplayeraction');
+            $("html,body").toggleClass('overflowhide');
+            
+            $("p").toggleClass('activep');
+
+            if(!($(".header").hasClass('activeheader'))){
+                $(".header").addClass('activeheader');
+            };
+        
+    });
 
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         var wHeight = $(window).height();
 
+        if(scroll>5){
+            $(".header").addClass("activeheader", 2000, "easeInOutQuad");
+        }else{
+            $(".header").removeClass("activeheader", 1000, "easeInOutQuad");
+        }
         
         if(scroll > (wHeight-200)){
             $(".toTop").addClass("show", 2000, "easeInOutQuad");
